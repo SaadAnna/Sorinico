631d64d9e53941dfbb08562657717f77e0badf7d
Saad Anna <saadannadev@gmail.com>
2025-05-05 09:29:13 +0200

    Update some style in the navbar section

--- app/DeutschlandCom .jsx
+++ app/DeutschlandCom .jsx
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 import IconWeb from "@/public/iconprincip.png";
-import Image from 'next/image';
+import Image from "next/image";
 const DeutschlandCom = () => {
   return (
-    <div className="max-w-7xl mx-auto lg:flex items-center gap-10">
-      {/* Header */}
-      <header className="mb-8">
-        <Image src={IconWeb} alt='Website Icon' width={50} height={50} />
-      </header>
-
-      {/* Casino Section */}
-      <section>
-        <h2 className="text-2xl font-semibold text-gray-700 mb-4">Beste Online Casinos</h2>
-        
-        {/* Navigation Links */}
-        <nav className="flex flex-wrap gap-4 md:gap-6">
-          <a href="#" className="text-black font-semibold text-lg">Bonus</a>
-          <a href="#" className="text-black font-semibold text-lg">Spiele</a>
-          <a href="#" className="text-black font-semibold text-lg">Zahlungsmethoden</a>
-          <a href="#" className="text-black font-semibold text-lg">News</a>
-        </nav>
-      </section>
-    </div>
+    <section className="max-w-7xl mx-auto lg:flex items-center lg:justify-between flex flex-col bg-white shadow-2xl shadow-black">
+      <div className="text-xl text-black text-bold">
+        <div>Deutschland.com</div>
+      </div>
+      <nav className="lg:flex items-center flex flex-col gap-4 md:gap-6">
+        <a href="#" className="text-black font-semibold text-lg p-4 transition-all ease-in hover:bg-gray-200">
+          Beste Online Casinos
+        </a>
+        <a href="#" className="text-black font-semibold text-lg p-4 transition-all ease-in hover:bg-gray-200">
+          Bonus
+        </a>
+        <a href="#" className="text-black font-semibold text-lg p-4 transition-all ease-in hover:bg-gray-200">
+          Spiele
+        </a>
+        <a href="#" className="text-black font-semibold text-lg p-4 transition-all ease-in hover:bg-gray-200">
+          Zahlungsmethoden
+        </a>
+        <a href="#" className="text-black font-semibold text-lg p-4 transition-all ease-in hover:bg-gray-200">
+          News
+        </a>
+      </nav>
+    </section>
   );
 };
 
